fad7111b201d1288495c83b7d4fac17a4929fc7a
Ashley Connor <ash.connor@gmail.com>
2012-04-07 20:17:31 +0100

    - Removed Print Model method - Removed Initialise Model method

--- src/animating/algorithms/KnapsackAlgorithm.java
+++ src/animating/algorithms/KnapsackAlgorithm.java
@@ -32,126 +32,113 @@ public final class KnapsackAlgorithm implements Runnable {
         this.capacity      = capacity;          //add one for blank row
         this.speed         = speed;
         
-        knapsackModel = createModelData();
+        //Create model to be used by tabls
+        knapsackModel = createDataModel();
         setColumnHeaders();
+        
+        //Assign model to table
         knapsackTableModel = new DefaultTableModel(knapsackModel, columnHeaders);
         table.setModel(knapsackTableModel);
         
     }
     
-        @Override
-      public void run() {
-            try {
-                // *** initialize
-                //intialiseModel();
-                numItems = value.size() - 1;
-                
-                sleepTime = speed * 100;
-                int actionTime = speed * 5;
+    @Override
+    public void run() {
+    try {
+        numItems = value.size() - 1;
 
-                //MESSAGE: Zero out the first row
-                textArea.setText("Zero out the first row.");
-                Thread.sleep(1);
-                
-                //zero out first row
-                for (int w = 0; w <= capacity; w++) {
-                    knapsackTableModel.setValueAt(new Integer(0), 0, w);
-                    Thread.sleep(actionTime);
+        sleepTime = speed * 100;
+        int actionTime = speed * 5;
+
+        //MESSAGE: Zero out the first row
+        textArea.setText("Zero out the first row.");
+        Thread.sleep(1);
+
+        for (int w = 0; w <= capacity; w++) {
+            knapsackTableModel.setValueAt(new Integer(0), 0, w);
+            Thread.sleep(actionTime);   //calm down dear it's an animation
+        }
+
+        knapsackTableModel.fireTableDataChanged();
+
+        //MESSAGE: Loop through all X items
+        textArea.setText("Loop through all " + numItems + " items");
+        //Thread.sleep(sleepTime);
+
+        // *** Now do the work!
+        for (int k = 1; k <= numItems; k++) {
+            System.out.println("Weight at " + k + ": " + weight.get(k));
+
+            //MESSAGE: The weight of item X is Y
+
+            for (int w = capacity; w >= weight.get(k); w--) {
+
+                //MESSAGE: Check if the item can fit in the weight.
+
+
+                if (value.get(k) + modelValueAt(k-1, w- weight.get(k)) > modelValueAt(k-1, w)) {
+                    //knapsackModel[k][w] = value.get(k) + knapsackModel[k - 1][w - weight.get(k)];
+
+                    knapsackTableModel.setValueAt(new Integer(value.get(k) + modelValueAt(k - 1, w - weight.get(k))),k,w);
+
+                    //MESSAGE: Since X is more than Y we use X
+
+                } else {
+                    //knapsackModel[k][w] = knapsackModel[k - 1][w];
+                    knapsackTableModel.setValueAt(knapsackTableModel.getValueAt(k-1, w), k, w);
+
+                    //MESSAGE: Since Y is bigger than X we use Y
                 }
-                
-                knapsackTableModel.fireTableDataChanged();
-
-                //MESSAGE: Loop through all X items
-                textArea.setText("Loop through all " + numItems + " items");
-                //Thread.sleep(sleepTime);
-
-                // *** Now do the work!
-                for (int k = 1; k <= numItems; k++) {
-                    System.out.println("Weight at " + k + ": " + weight.get(k));
-
-                    //MESSAGE: The weight of item X is Y
-
-                    for (int w = capacity; w >= weight.get(k); w--) {
-
-                        //MESSAGE: Check if the item can fit in the weight.
-                        
-                        int a = (Integer) knapsackTableModel.getValueAt(k - 1, w - weight.get(k));
-                        int b = (Integer) knapsackTableModel.getValueAt(k - 1, w);
-
-                        if (value.get(k) + a > b) {
-                            //knapsackModel[k][w] = value.get(k) + knapsackModel[k - 1][w - weight.get(k)];
-                            
-                            int c = (Integer) knapsackTableModel.getValueAt(k - 1, w - weight.get(k));
-                            
-                            knapsackTableModel.setValueAt(new Integer(value.get(k) + c),k,w);
-
-                            //MESSAGE: Since X is more than Y we use X
-
-                        } else {
-                            //knapsackModel[k][w] = knapsackModel[k - 1][w];
-                            knapsackTableModel.setValueAt(knapsackTableModel.getValueAt(k-1, w), k, w);
-
-                            //MESSAGE: Since Y is bigger than X we use Y
-                        }
-                    }
-                    for (int w = 0; w < weight.get(k); w++) {
-                        //knapsackModel[k][w] = knapsackModel[k - 1][w];
-                        knapsackTableModel.setValueAt(knapsackTableModel.getValueAt(k-1, w), k, w);
-
-                        //MESSAGE: We take hte lar
-                    }
-                }
-            
-            } catch (InterruptedException ex) {
-                System.out.println("Interrupted");
             }
-            
-            //Clear arrays for new values
-            value.clear();
-            weight.clear();
-            //printModel();
+            for (int w = 0; w < weight.get(k); w++) {
+                //knapsackModel[k][w] = knapsackModel[k - 1][w];
+                knapsackTableModel.setValueAt(knapsackTableModel.getValueAt(k-1, w), k, w);
+
+                //MESSAGE: ???
+            }
+        }
+
+    } catch (InterruptedException ex) {
+        
+        System.out.println("Interrupted.");
+    
+    }
+
+        //Clear arrays for new values
+        value.clear();
+        weight.clear();
     }
       
       //why there is no standard library method to do this!!!?
-      public Integer[][] createModelData() {
+    
+    //Gets the value at column x, row y and returns an int
+    private int modelValueAt(int x, int y) {
+
+        return (Integer) knapsackTableModel.getValueAt(x,y);
+    }
+
+    //creates a model with capacity * columns and numItems * rows
+    private Integer[][] createDataModel() {
           
           knapsackModel = new Integer[value.size()][capacity + 1];
           
           return knapsackModel;
       }
       
-      public void setColumnHeaders() {
-          
-          columnHeaders = new String[capacity + 1];
-          
-          for (int i = 0; i <= capacity; i++) {
-              
-              columnHeaders[i] = i + "";
-          }
-      }
-      
-      public void intialiseModel() {
-          
-          numItems = value.size() - 1;
-          
-          for (int i = 0; i < knapsackModel.length; i++) {
-              for (int j = 0; j < knapsackModel[i].length; j++) {
-                  knapsackModel[i][j] = new Integer(0);
-              }
-          }
-      }
-      
-      public void printModel() {
-          
-          for (int i = 0; i < knapsackModel.length; i++) {
-              for (int j = 0; j < knapsackModel[i].length; j++) {
-                  System.out.println(knapsackModel[i][j]);
-              }
-          }
-      }
-      
+    //set the value of the column headers 1...capacity
+    private void setColumnHeaders() {
+
+        columnHeaders = new String[capacity + 1];
+
+        for (int i = 0; i <= capacity; i++) {
+
+            columnHeaders[i] = i + "";
+        }
+    }
+
+    //set the speed of the animation by changing the sleep ms value
     public void setSpeed(int speed) {
-        
+
         this.speed = speed;
     }
 }
