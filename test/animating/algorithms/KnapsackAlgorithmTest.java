186c6c3f6adce5aaf4d42fbb4990cf1b5a721a46
Ashley Connor <ash.connor@gmail.com>
2012-05-09 22:12:33 +0100

    - Added first JUnit test

---
+++ test/animating/algorithms/KnapsackAlgorithmTest.java
@@ -0,0 +1,55 @@
+package animating.algorithms;
+
+import static org.junit.Assert.fail;
+import org.junit.*;
+
+/**
+ *
+ * @author Ashley Connor <ash.connor@gmail.com>
+ */
+public class KnapsackAlgorithmTest {
+    
+    public KnapsackAlgorithmTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of run method, of class KnapsackAlgorithm.
+     */
+    @Test
+    public void testRun() {
+        System.out.println("run");
+        KnapsackAlgorithm instance = null;
+        instance.run();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setSpeed method, of class KnapsackAlgorithm.
+     */
+    @Test
+    public void testSetSpeed() {
+        System.out.println("setSpeed");
+        int speed = 0;
+        KnapsackAlgorithm instance = null;
+        instance.setSpeed(speed);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+}
